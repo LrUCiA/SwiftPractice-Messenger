4d8ccd8096a380872190c1ec784dea314e81ff5d
LrUCiA <crosshertz7@gmail.com>
2022-01-06 00:29:42 +0900

    .

---
+++ Messenger/Resources/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Messenger
+//
+//  Created by LrUCiA.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        self.frame.size.width   
+    }
+}
