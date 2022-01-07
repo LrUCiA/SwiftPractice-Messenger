5a658454fa41d3d4a60cac58f15f7e531e189741
LrUCiA <crosshertz7@gmail.com>
2022-01-07 21:59:54 +0900

    .

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -16,6 +16,14 @@ class LoginViewController: UIViewController {
         return scrollView
     }()
     
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    
     private let emailField: UITextField = {
         let field = UITextField()
         field.autocapitalizationType = .none
@@ -35,7 +43,7 @@ class LoginViewController: UIViewController {
         let field = UITextField()
         field.autocapitalizationType = .none
         field.autocorrectionType = .no
-        field.returnKeyType = .continue
+        field.returnKeyType = .done
         field.layer.cornerRadius = 12
         field.layer.borderWidth = 1
         field.layer.borderColor = UIColor.lightGray.cgColor
@@ -46,13 +54,16 @@ class LoginViewController: UIViewController {
         field.isSecureTextEntry = true //입력 글자를 *로 표시
         return field
     }()
-
     
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "logo")
-        imageView.contentMode = .scaleAspectFit
-        return imageView
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
     }()
 
     override func viewDidLoad() {
@@ -64,11 +75,20 @@ class LoginViewController: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
+        
+        loginButton.addTarget(self,
+                              action: #selector(loginButtonTapped),
+                              for: .touchUpInside)
+        
+        emailField.delegate = self
+        passwordField.delegate = self
+        
        //Add Subviews
         view.addSubview(scrollView)
         scrollView.addSubview(imageView)
         scrollView.addSubview(emailField)
         scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
     }
     
     override func viewDidLayoutSubviews() {
@@ -87,7 +107,35 @@ class LoginViewController: UIViewController {
                                   y: emailField.bottom+10,
                                   width: scrollView.width-60,
                                  height: 52)
+        loginButton.frame = CGRect(x: 30,
+                                  y: passwordField.bottom+10,
+                                  width: scrollView.width-60,
+                                 height: 52)
     }
+    
+    @objc private func loginButtonTapped() { //로그인 유효성 검사
+        
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+        
+        guard let email = emailField.text, let password = passwordField.text,
+              !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+                  alertUserLoginError()
+                  return
+              }
+        
+        //Firebase Log In
+        
+    }
+    
+    func alertUserLoginError() {
+        let alert = UIAlertController(title: "Oops!",
+                                      message: "Please enter all information to log in.",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss",
+                                      style: .cancel, handler: nil))
+        present(alert, animated: true)
+        }
     
     @objc private func didTapRegister() {
         let vc = RegisterViewController()
@@ -95,4 +143,19 @@ class LoginViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
 
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == emailField {
+            passwordField.becomeFirstResponder()
+        } else if textField == passwordField {
+            loginButtonTapped()
+        }
+        
+        return true
+    }
+    
 }
