52a4e713a06cda052043d1fb0939b5b4af72877e
LrUCiA <crosshertz7@gmail.com>
2022-02-04 18:48:06 +0900

    .

--- Messenger/Controllers/Login/RegisterViewController.swift
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -201,27 +201,27 @@ class RegisterViewController: UIViewController {
             guard let strongSelf = self else {
                 return
             }
+            
             guard !exists else {
                 // user already exists
                 strongSelf.alertUserLoginError(message: "Looks like a user account for that email address already exists.")
                 return
             }
-        })
-        
-        FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { authResult, error in
             
-            guard authResult != nil, error == nil else {
-                print("Error creating user")
-                return
-            }
-            
-            DatabaseManager.shared.insertUser(with: ChatAppUser(firstName: firstName,
-                                                                lastName: lastName,
-                                                                emailAddress: email))
-            strongSelf.navigationController?.dismiss(animated: true, completion: nil)
+            FirebaseAuth.Auth.auth().createUser(withEmail: email, password: password, completion: { authResult, error in
+                guard authResult != nil, error == nil else {
+                    print("Error creating user.")
+                    return
+                }
+                
+                DatabaseManager.shared.insertUser(with: ChatAppUser(firstName: firstName,
+                                                                    lastName: lastName,
+                                                                    emailAddress: email))
+                strongSelf.navigationController?.dismiss(animated: true, completion: nil)
+            })
         })
     }
-    
+
     func alertUserLoginError(message: String = "Please enter all information to create a new account.") {
         let alert = UIAlertController(title: "Oops!",
                                       message: message,
